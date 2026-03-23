27d39198abfc8aa1bcd4ca49cbcdee954b0501c7
Rafael Debner <111936927+RDebner@users.noreply.github.com>
2026-03-23 20:04:02 -0300

    Add files via upload

---
+++ ex1/ex1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var lado1, lado2, lado3
+
+	fmt.Print("Informe os lados do triângulo: ")
+	fmt.Scanf("%v", lado1, lado2, lado3)
+	fmt.Print(lado1, lado2, lado3)
+}
