7e97be063e6b02a864a76a246d23510a360d67cf
Rafael Debner <111936927+RDebner@users.noreply.github.com>
2026-03-25 20:04:17 -0300

    Update ex3.go

--- Sala de aula/Exercícios em sala/ex3.go
+++ Sala de aula/Exercícios em sala/ex3.go
@@ -2,17 +2,17 @@ package main
 
 import "fmt"
 
-var qntNotas int
+var qntAlunos int
 var soma float64 = 0
 var nota float64 
 
 func main() {
-	fmt.Print("Quantidade de notas: ")
-	fmt.Scan(&qntNotas)
-	for i := 0; i < qntNotas; i++ {
-		fmt.Printf("Escreva a nota %d:\n", i + 1)
+	fmt.Print("Quantidade de alunos: ")
+	fmt.Scan(&qntAlunos)
+	for i := 0; i < qntAlunos; i++ {
+		fmt.Printf("Escreva a nota do aluno %d:\n", i + 1)
 		fmt.Scan(&nota)
 		soma += nota
 	}
-	fmt.Println(soma/float64(qntNotas))
+	fmt.Println(soma/float64(qntAlunos))
 }
